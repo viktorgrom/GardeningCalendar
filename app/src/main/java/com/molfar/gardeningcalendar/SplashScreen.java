202453f6a43888657ce1996fa1a84606301ec7f3
viktorgrom <0673503163v@gmail.com>
2021-02-05 20:41:43 +0200

    add catgs 7,8

--- app/src/main/java/com/molfar/gardeningcalendar/SplashScreen.java
+++ app/src/main/java/com/molfar/gardeningcalendar/SplashScreen.java
@@ -2,7 +2,6 @@ package com.molfar.gardeningcalendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,7 +12,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_splash_screen);
+
 
         EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
                 .withFullScreen()
@@ -21,16 +20,16 @@ public class SplashScreen extends AppCompatActivity {
                 .withSplashTimeOut(4000)
                 .withLogo(R.drawable.soroka_logo_mini)
                 .withBackgroundResource(R.drawable.background_start2)
-                //.withBackgroundColor(Color.parseColor("#074E72"))
+
                 .withHeaderText("Студія розробки MOLFAR презентує")
                 .withBeforeLogoText("Календар Садівника");
 
 
         config.getHeaderTextView().setTextColor(0xFFF0932B);
         config.getBeforeLogoTextView().setTextSize(30);
-        //config.getFooterTextView().setTextColor(0xFFF0932B);
+
         config.getBeforeLogoTextView().setTextColor(0xFFF0932B);
-        //config.getAfterLogoTextView().setTextColor(0xFFF0932B);
+
 
         //set to view
         View view = config.create();
